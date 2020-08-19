d0dc7a28854cd8311d073b270cd3093868236d66
“WesleyMeneghini” <wesley.meneghini@outlook.com>
2020-08-19 10:11:58 -0300

    created document Swagger

--- src/main/java/com/brasilprev/desafio/resource/ClientResource.java
+++ src/main/java/com/brasilprev/desafio/resource/ClientResource.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +24,8 @@ import com.brasilprev.desafio.model.Address;
 import com.brasilprev.desafio.model.Client;
 import com.brasilprev.desafio.repository.AddressRepository;
 import com.brasilprev.desafio.repository.ClientRepository;
+
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/clients")
@@ -33,19 +37,22 @@ public class ClientResource {
 	@Autowired
 	AddressRepository addressRepository;
 	
+	@ApiOperation(value = "Returns all clients")
 	@GetMapping
 	public List<Client> getClients(){
 		return clientRepository.findAll();
 	}
 	
-	@GetMapping("/{id}")
+	@ApiOperation(value = "Returns the client by id")
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
 	public ResponseEntity<?> getClientById(@PathVariable Long id){
 		Client client = clientRepository.findById(id).orElse(null);
 		if(client == null) ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		return ResponseEntity.ok(client);
 	}
 	
-	@PostMapping
+	@ApiOperation(value = "Register new client")
+	@PostMapping(produces = "application/json")
 	public  ResponseEntity<?> create(@Valid @RequestBody Client client) {
 		if(clientRepository.existsByCpf(client.getCpf())) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
@@ -53,6 +60,7 @@ public class ClientResource {
 		return ResponseEntity.status(HttpStatus.CREATED).body(clientRepository.save(client));
 	}
 	
+	@ApiOperation(value = "Update client")
 	@PutMapping
 	public ResponseEntity<?> update(@Validated @RequestBody Client client) {
 		
@@ -63,6 +71,7 @@ public class ClientResource {
 		return ResponseEntity.status(HttpStatus.OK).body(clientRepository.save(client));
 	}
 	
+	@ApiOperation(value = "Update address the client")
 	@PutMapping("/{idClient}/address")
 	public ResponseEntity<?> updateAddress(@Validated @RequestBody Address address, @PathVariable Long idClient) {
 		
@@ -73,6 +82,7 @@ public class ClientResource {
 		return ResponseEntity.status(HttpStatus.OK).body(addressRepository.save(address));
 	}
 	
+	@ApiOperation(value = "Delete client")
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public ResponseEntity<?> delete(@PathVariable Long id) {
